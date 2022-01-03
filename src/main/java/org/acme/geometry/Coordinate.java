cc106232d58664a6990ab819c0152e6d3d79ed12
Amaryllis25 <amaryllis.vignaud@ensg.eu>
2022-01-03 14:09:57 +0100

    Add Coordinate class

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,27 @@
 package org.acme.geometry;
 
 public class Coordinate {
+	
+	double x;
+	double y;
 
+	//getter
+    public double getX() {
+        return this.x;
+    }
+
+    public double getY() {
+        return this.y;
+    }
+    
+    //method point
+    public Coordinate() {
+    	this.x = 0.0;
+    	this.y = 0.0;
+    	}
+    
+    public Coordinate(double x, double y) {
+    	this.x = x;
+    	this.y = y;
+    	}
 }
