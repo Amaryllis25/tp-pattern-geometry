a6025b01684d1a26a189e9d083fca19a74636b5a
Amaryllis25 <79219571+Amaryllis25@users.noreply.github.com>
2022-01-03 16:27:10 +0100

    Update LineStringTest.java
    Forget push Q0.3

--- src/test/java/org/acme/geometry/LineStringTest.java
+++ src/test/java/org/acme/geometry/LineStringTest.java
@@ -12,6 +12,44 @@ public class LineStringTest {
 	
     @Test
     public void testDefaultConstructor() {
-        //LineString line = new LineString();
+        LineString line = new LineString();
+        Assert.assertEquals(0, line.getNumPoints());
+        Assert.assertTrue(line.isEmpty());
     }
+    
+    @Test
+    public void testDefaultConstructor2() {
+        List<Point> points = new ArrayList<Point>();
+        Coordinate c = new Coordinate(1.1, 1.2);
+        Point p = new Point(c);
+        points.add(p);
+
+        LineString line = new LineString(points);
+        Assert.assertEquals(p, line.getPointN(0));
+		Assert.assertFalse(line.isEmpty());
+    }
+    
+    @Test
+    public void testType() {
+    	LineString line = new LineString();
+    	Assert.assertEquals("LineString", line.getType());
+    }
+    
+    @Test
+    public void testConstructorSizeAdd() {
+        List<Point> points = new ArrayList<Point>();
+        Coordinate c = new Coordinate(1.1, 1.2);
+        Coordinate c2 = new Coordinate(1.3, 1.4);
+        
+        Point p = new Point(c);
+        Point p2 = new Point(c2);
+        
+        points.add(p);
+        points.add(p2);
+
+        LineString line = new LineString(points);
+        Assert.assertEquals(2, line.getNumPoints());
+		Assert.assertFalse(line.isEmpty());
+    }
+    
 }
