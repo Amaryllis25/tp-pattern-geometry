d078384321cad129babb9bacb65ee8412ae08236
Amaryllis25 <amaryllis.vignaud@ensg.eu>
2022-01-04 16:43:10 +0100

    Add tests and file AbstractGeometryTest

---
+++ src/test/java/org/acme/geometry/AbstractGeometryTest.java
@@ -0,0 +1,19 @@
+package org.acme.geometry;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AbstractGeometryTest {
+	
+	@Test
+	public void testTextMethodPoint(){
+		Point p = SampleFactory.createPointO();
+		Assert.assertEquals("POINT(1.1 1.2)", p.asText());
+	}
+	
+	@Test
+	public void testTextMethodineString(){
+		LineString line = SampleFactory.createLineStringOA();
+		Assert.assertEquals("LINESTRING(1.1 1.2,6.23 23.6)", line.asText());
+	}
+}
