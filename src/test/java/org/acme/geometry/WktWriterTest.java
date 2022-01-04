3906805946ab6fbf2bc1862676c7a9fa5659c70c
Amaryllis25 <amaryllis.vignaud@ensg.eu>
2022-01-04 12:30:42 +0100

    Add first part of tests

--- src/test/java/org/acme/geometry/WktWriterTest.java
+++ src/test/java/org/acme/geometry/WktWriterTest.java
@@ -1,6 +1,31 @@
 package org.acme.geometry;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class WktWriterTest {
+
+	@Test
+	public void testWriteMethodWithEmptyPoint() {
+		
+		WktWriter wkt = new WktWriter();
+		Point p = new Point();
+		
+		String str = wkt.write(p);
+		
+		Assert.assertEquals("POINT EMPTY", str);
+	}
 	
+	
+	@Test
+	public void testWriteLineEmptyString() {
+		
+		WktWriter wkt = new WktWriter();
+		LineString line = new LineString();
+		
+		String str = wkt.write(line);
+		
+		Assert.assertEquals("LINESTRING EMPTY",str);
+	}
 
 }
