60c66f7e4c88044a56890217feed63c872ce1fd2
Amaryllis25 <amaryllis.vignaud@ensg.eu>
2022-01-04 11:45:18 +0100

    Complet coverage test

--- src/test/java/org/acme/geometry/EnvelopeBuilderTest.java
+++ src/test/java/org/acme/geometry/EnvelopeBuilderTest.java
@@ -8,7 +8,7 @@ public class EnvelopeBuilderTest {
 	public static final double EPSILON = 1.0e-15;
 	
 	@Test
-	public void testDefaultConstructor() {
+	public void testDefaultConstructorX() {
 		
 		EnvelopeBuilder builder = new EnvelopeBuilder();
 		builder.insert(new Coordinate(1.1, 1.2));
@@ -19,6 +19,51 @@ public class EnvelopeBuilderTest {
         Assert.assertEquals(0.0, envelope.getXmin(), EPSILON);
         Assert.assertEquals(0.0, envelope.getYmin(), EPSILON);
         Assert.assertEquals(6.23, envelope.getXmax(), EPSILON);
+        Assert.assertEquals(23.6, envelope.getYmax(), EPSILON);
+	}
+	
+	@Test
+	public void testDefaultConstructorY() {
+		
+		EnvelopeBuilder builder = new EnvelopeBuilder();
+		builder.insert(new Coordinate(1.1, 1.2));
+		builder.insert(new Coordinate(6.23, 23.6));
+		builder.insert(new Coordinate(10, 30));
+		Envelope envelope = builder.build();
+		
+        Assert.assertEquals(1.1, envelope.getXmin(), EPSILON);
+        Assert.assertEquals(1.2, envelope.getYmin(), EPSILON);
+        Assert.assertEquals(10, envelope.getXmax(), EPSILON);
+        Assert.assertEquals(30, envelope.getYmax(), EPSILON);
+	}
+	
+	@Test
+	public void testDefaultConstructorXNotY() {
+		
+		EnvelopeBuilder builder = new EnvelopeBuilder();
+		builder.insert(new Coordinate(1.1, 1.2));
+		builder.insert(new Coordinate(6.23, 23.6));
+		builder.insert(new Coordinate(0, 30));
+		Envelope envelope = builder.build();
+		
+        Assert.assertEquals(0, envelope.getXmin(), EPSILON);
+        Assert.assertEquals(1.2, envelope.getYmin(), EPSILON);
+        Assert.assertEquals(6.23, envelope.getXmax(), EPSILON);
+        Assert.assertEquals(30, envelope.getYmax(), EPSILON);
+	}
+	
+	@Test
+	public void testDefaultConstructorYnotX() {
+		
+		EnvelopeBuilder builder = new EnvelopeBuilder();
+		builder.insert(new Coordinate(1.1, 1.2));
+		builder.insert(new Coordinate(6.23, 23.6));
+		builder.insert(new Coordinate(10, 0));
+		Envelope envelope = builder.build();
+		
+        Assert.assertEquals(1.1, envelope.getXmin(), EPSILON);
+        Assert.assertEquals(0, envelope.getYmin(), EPSILON);
+        Assert.assertEquals(10, envelope.getXmax(), EPSILON);
         Assert.assertEquals(23.6, envelope.getYmax(), EPSILON);
 	}
 
