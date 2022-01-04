27dcc10fe560731150774334c36fb682fd0b66c1
Amaryllis25 <amaryllis.vignaud@ensg.eu>
2022-01-04 12:24:03 +0100

    Complete WktWriter class

--- src/main/java/org/acme/geometry/WktWriter.java
+++ src/main/java/org/acme/geometry/WktWriter.java
@@ -1,5 +1,42 @@
 package org.acme.geometry;
 
 public class WktWriter {
+	
+	public WktWriter() {
+	}
+	
+    public String write(Geometry geometry){
+    	
+        String wkt = "";
+
+        if (geometry instanceof Point){
+            Point point = (Point)geometry;
+            if (point.isEmpty()) {
+            	wkt = "POINT EMPTY";
+            }
+            else {
+            	wkt = "POINT(" + point.getCoordinate().getX() + " " + point.getCoordinate().getY() + ")";
+            }
+        }
+        else if (geometry instanceof LineString){
+            LineString line = (LineString)geometry;
+            if (line.isEmpty()) {
+            	wkt = "LINESTRING EMPTY";
+            }
+            else {
+                Point Point1 = line.getPointN(0);
+                wkt = "LINESTRING(" + Point1.getCoordinate().getX() + " " + Point1.getCoordinate().getY();
+                for (int i = 1; i < line.getNumPoints(); i++){
+                    Point p = line.getPointN(i);
+                    wkt += "," + p.getCoordinate().getX() + " " + p.getCoordinate().getY();
+                }
+                wkt += ")";
+            }
+        }
+        else{
+            throw new RuntimeException("geometry type not supported");
+        }
+        return wkt;
+    }
 
 }
