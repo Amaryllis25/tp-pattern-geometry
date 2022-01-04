b1068fd8b61ae955bd7e695d2b13fbb82f0569ac
Amaryllis25 <79219571+Amaryllis25@users.noreply.github.com>
2022-01-04 09:41:53 +0100

    Update LineString.java

--- src/main/java/org/acme/geometry/LineString.java
+++ src/main/java/org/acme/geometry/LineString.java
@@ -33,6 +33,6 @@ public class LineString implements Geometry{
 	
     @Override
     public boolean isEmpty() {
-        return this.getNumPoints() == 0;
+        return this.points.isEmpty();
     }
 }
