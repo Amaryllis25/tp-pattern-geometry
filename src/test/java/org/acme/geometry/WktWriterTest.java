ddf145f3a95902e1077d37a743b293b989725b62
Amaryllis25 <amaryllis.vignaud@ensg.eu>
2022-01-04 13:46:44 +0100

    Add test

--- src/test/java/org/acme/geometry/WktWriterTest.java
+++ src/test/java/org/acme/geometry/WktWriterTest.java
@@ -27,5 +27,26 @@ public class WktWriterTest {
 		
 		Assert.assertEquals("LINESTRING EMPTY",str);
 	}
+	
+	@Test
+	public void testWritePoint() {
 
+		WktWriter wkt = new WktWriter();
+		Point M = SampleFactory.createPointO();
+		
+		String str = wkt.write(M);
+		
+		Assert.assertEquals("POINT(1.1 1.2)", str);
+	}
+	
+	@Test
+	public void testWriteLinestringOM() {
+
+		WktWriter wkt = new WktWriter();
+		LineString line = SampleFactory.createLineStringOA();
+		
+		String str = wkt.write(line);
+				
+		Assert.assertEquals("LINESTRING(1.1 1.2,6.23 23.6)",str);
+	}
 }
