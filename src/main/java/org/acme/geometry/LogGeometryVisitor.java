dacbd09bb8cd3daf8405f0ed1d6528c20a7db7aa
Amaryllis25 <amaryllis.vignaud@ensg.eu>
2022-01-04 14:17:50 +0100

    Complete class and interface files for GeometryVisitor

--- src/main/java/org/acme/geometry/LogGeometryVisitor.java
+++ src/main/java/org/acme/geometry/LogGeometryVisitor.java
@@ -1,5 +1,25 @@
 package org.acme.geometry;
 
-public class LogGeometryVisitor {
+import java.io.PrintStream;
+
+public class LogGeometryVisitor implements GeometryVisitor{
+	
+	private PrintStream out;
+	
+    public LogGeometryVisitor() {
+        this.out = System.out;
+    }
+	
+	public LogGeometryVisitor(PrintStream out) {
+		this.out = out;
+	}
+	
+	public void visit(Point point) {
+		out.println("Je suis un point avec x=" + point.getCoordinate().getX() + " et y="+ point.getCoordinate().getY());
+	}
+	
+	public void visit(LineString lineString) {
+		out.println("Je suis une polyligne définie par"+ lineString.getNumPoints() + "point(s)");
+	}
 
 }
